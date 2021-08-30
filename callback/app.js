cf91b6945e560e49882042baed90e6178215f90a
Oli Ullah Jewel <oliullahjewel7@gmail.com>
2021-08-30 19:53:39 +0600

    Callback functions

--- callback/app.js
+++ callback/app.js
@@ -1,2 +1,43 @@
- 
- 
+const Posts = [
+    { title: " Post one", body: "This is the post one" },
+    { title: " Post two", body: "This is the post two" },
+];
+
+// function createPost(post) {
+//     setTimeout(function() {
+//         Posts.push(post);
+//     }, 2000);
+// }
+
+// function getPosts() {
+//     setTimeout(function() {
+//         let output = "";
+//         Posts.forEach(function(post) {
+//             output += `<li>${post.title}</li>`;
+//         });
+//         document.body.innerHTML = output;
+//     }, 1000);
+// }
+
+// createPost({ title: "Post three", body: "This is the post three" });
+
+// getPosts();
+
+function createPost(post, callback) {
+    setTimeout(function() {
+        Posts.push(post);
+        callback();
+    }, 2000);
+}
+
+function getPosts() {
+    setTimeout(function() {
+        let output = "";
+        Posts.forEach(function(post) {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+createPost({ title: "Post three", body: "This is the post three" }, getPosts);
